91cc75d95d788917357e18103e5c38924bd20a34
ZeloZalis <117476567+ZeloZalis@users.noreply.github.com>
2024-01-26 23:26:07 -0400

    Modal and optimization
    Added modal test command and optimizes commands in cogs

--- cogs/gifs_commands.py
+++ cogs/gifs_commands.py
@@ -2,50 +2,49 @@ import random
 import discord
 from discord.ext import commands
 
-client = discord.Bot()
 testing_server = [522277286024708096, 574449304832311297]
 
 class Gifs(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @client.slash_command(guild_ids=testing_server, name="urielito", description="Publica un gif de Uriel.")
+    @commands.slash_command(guild_ids=testing_server, name="urielito", description="Publica un gif de Uriel.")
     async def Urielito(self, ctx):
         with open(r"resources\urielito.txt", "r") as file:
             response_list = file.readlines()
             response = random.choice(response_list)
         await ctx.respond(response)
 
-    @client.slash_command(guild_ids=testing_server, name="yes", description="Un mensaje para ye.")
+    @commands.slash_command(guild_ids=testing_server, name="yes", description="Un mensaje para ye.")
     async def Yes(self, ctx):
         await ctx.respond("Ye puto.")
     
-    @client.slash_command(guild_ids=testing_server, name="freddy", description="Publica un gif de Freddy.")
+    @commands.slash_command(guild_ids=testing_server, name="freddy", description="Publica un gif de Freddy.")
     async def Freddy(self, ctx):
         with open(r"resources\freddy.txt", "r") as file:
             response_list = file.readlines()
             response = random.choice(response_list)
         await ctx.respond(response)
 
-    @client.slash_command(guild_ids=testing_server, name="luí", description="Publica un gif de Luí.")
+    @commands.slash_command(guild_ids=testing_server, name="luí", description="Publica un gif de Luí.")
     async def Luí(self, ctx):
         await ctx.respond("https://c.tenor.com/yvdqdIeLhFMAAAAd/tenor.gif")
     
-    @client.slash_command(guild_ids=testing_server, name="payito", description="Publica un gif de Payito.")
+    @commands.slash_command(guild_ids=testing_server, name="payito", description="Publica un gif de Payito.")
     async def Payito(self, ctx):
         with open(r"resources\payito.txt", "r") as file:
             response_list = file.readlines()
             response = random.choice(response_list)
         await ctx.respond(response)
 
-    @client.slash_command(guild_ids=testing_server, name="ehlipin", description="Publica un gif de Ehlipin.")
+    @commands.slash_command(guild_ids=testing_server, name="ehlipin", description="Publica un gif de Ehlipin.")
     async def Ehlipin(self, ctx):
         with open(r"resources\ehlipin.txt", "r") as file:
             response_list = file.readlines()
             response = random.choice(response_list)
         await ctx.respond(response)
 
-    @client.slash_command(guild_ids=testing_server, name="yoelito", description="Publica un gif de Yoelito.")
+    @commands.slash_command(guild_ids=testing_server, name="yoelito", description="Publica un gif de Yoelito.")
     async def Yoelito(self, ctx):
         with open(r"resources\yoelito.txt", "r") as file:
             response_list = file.readlines()
--- cogs/utility_commands.py
+++ cogs/utility_commands.py
@@ -1,16 +1,29 @@
 import random
 import discord
 from discord.ext import commands
+from discord.ui import InputText, Modal
 
-client = discord.Bot()
 testing_server = [522277286024708096, 574449304832311297]
+
+#Creando un modal
+class MyModal(Modal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) #Título en la parte de arriba
+        self.add_item(InputText(label="Short Input", placeholder="Placeholder"))
+        self.add_item(InputText(label="Long Input", value="Default", style=discord.InputTextStyle.long)) #Puede ser long o short
+    
+    async def callback(self, interaction:discord.Interaction):
+        embed = discord.Embed(title="Resultado del Modal", color=discord.Color.green())
+        embed.add_field(name="First Input", value=self.children[0].value, inline=False)
+        embed.add_field(name="Second Input", value=self.children[1].value, inline=False)
+        await interaction.response.send_message(embeds=[embed])
 
 class Utility(commands.Cog):
     def __init__(self, client):
         self.client = client
     
     #Comando de ayuda, envía un enved con la información de los comandos
-    @client.slash_command(name="help", description="Muestra los comandos del bot.")
+    @commands.slash_command(name="help", description="Muestra los comandos del bot.")
     async def Help(self, ctx):
         help_embed = discord.Embed(
             title = "Tuttli comandos!",
@@ -25,7 +38,7 @@ class Utility(commands.Cog):
         await ctx.respond(embed=help_embed)
 
     #Lanza un dado del 1 al 6
-    @client.slash_command(guild_ids=testing_server, name="roll", description="Tira un dado de 6 caras.")
+    @commands.slash_command(guild_ids=testing_server, name="roll", description="Tira un dado de 6 caras.")
     async def Roll(self, ctx):
         num = random.randint(1, 6)
         dice_embed = discord.Embed(
@@ -35,6 +48,22 @@ class Utility(commands.Cog):
         dice_embed.set_author(name=f"¡{ctx.author.name} ha lanzado los dados!", icon_url=ctx.author.avatar)
         dice_embed.set_image(url="https://c.tenor.com/FOvBc0i9ZDcAAAAC/tenor.gif")
         await ctx.respond(embed=dice_embed)
+
+    #Borra una cantidad de mensajes del chat
+    @commands.slash_command(guild_ids=testing_server, name="clear", description="Borra una cantidad de mensajes del chat.")
+    @commands.has_role("Mod")
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, count: int):
+        await ctx.channel.purge(limit=count+1)
+        await ctx.respond(f"{count} mensaje(s) eliminados del chat.")
+
+    #Lo siguiente es un Modal, un pequeño
+    #Fichero que se puede rellenar, actualmente no tiene uso
+    #Para este bot
+    @commands.slash_command(guild_ids=testing_server, name="modal", description="Esto es un modal.")
+    async def modal(self, ctx:discord.ApplicationContext):
+        modal = MyModal(title="Modal via slash command")
+        await ctx.send_modal(modal)
 
 def setup(client):
     client.add_cog(Utility(client))
