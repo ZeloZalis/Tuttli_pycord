7d5a3bd63f74ff114ece43b655c155b45f9f8091
ZeloZalis <117476567+ZeloZalis@users.noreply.github.com>
2024-04-17 12:47:07 -0400

    Update
    Se ha creado la clase que muestra el View y recibe la lista con los diccionarios.

--- attacklistcheck.py
+++ attacklistcheck.py
@@ -1,6 +1,9 @@
 import random
-with open("resources/battle/skills.txt") as file:
+with open("resources/battle/skills.txt", encoding="utf-8") as file:
     attack_list = []
     read_lines = file.readlines()
     attack_list = random.sample(read_lines, 3)
-    print(f"{attack_list}")
+
+#Transformo la lista de cadenas en una lista con diccionarios
+diccio = [eval(i) for i in attack_list]
+print(diccio)
